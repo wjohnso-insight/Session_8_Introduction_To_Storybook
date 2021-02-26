ade9707b19020a1af7dee7a609721270d54af57b
Will <will.johnston@insight.com>
2021-02-26 14:48:53 -0500

    Added text input
    1. Created TextInput component
    2. Created primary story
    3. Want to be able to dispatch function using useReducer hook

--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { Button } from './Button';
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
   title: 'Example/Button',
   component: Button,
